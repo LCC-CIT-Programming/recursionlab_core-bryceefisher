f4a9bdbde6fcd16721eb77c5969753f5e754d53a
Bryce Fisher <bryce.e.fisher@gmail.com>
2023-04-21 14:43:32 -0700

    Completed Lab Q 1-2

--- Recursion_Core5/FibonacciNumbers/Program.cs
+++ Recursion_Core5/FibonacciNumbers/Program.cs
@@ -6,7 +6,34 @@ namespace FibonacciNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //for loop to iterate through the first 10 fibonacci numbers
+            for (int i = 0; i < 10; i++)
+            {
+                //display the 10 numbers to the console
+                Console.WriteLine($"Fib Number {i}: {FibNums(i)}");
+            }
         }
+       // method to determine the first 10 numbers in the fibonacci sequence
+        public static int FibNums(int n)
+        {
+            //if n is equal to 0 return 0
+            if (n == 0)
+            {
+                return 0;
+            }
+            // else if n is equal to 1 return 1
+            else if (n == 1)
+            {
+                return 1;
+            }
+            //else return the current value of n by calling the method on n-1 and n-2
+            else
+            {
+                return n = FibNums(n-1) + FibNums(n-2);
+            }
+        }
+
     }
+    
 }
+
--- Recursion_Core5/TriangularNumbers/Program.cs
+++ Recursion_Core5/TriangularNumbers/Program.cs
@@ -6,7 +6,30 @@ namespace TriangularNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //For loop to call triangular nums for the first 10 triangular numbers
+            for (int i = 1; i < 11; i++)
+            {
+                //Write nums to the console
+                Console.WriteLine($"Triangular Number {i}: {TriangularNums(i)}");
+            }
         }
+        //Method to determine triangular nums
+        public static int TriangularNums(int n)
+        {
+            //if n is equal to 1 return 1
+            if (n == 1)
+            {
+                return 1;
+            }
+            //else return the current value of n plus a recursive call to the method with n-1 as the param
+            else
+            {
+                return n += TriangularNums(n - 1);
+            }
+        }
+
     }
+    
 }
+
+
